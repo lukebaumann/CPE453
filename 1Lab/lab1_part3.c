b7c57c8db45bec466389954d9476ceb0793cd69d
Luke Baumann <mydoghasissues@gmail.com>
2014-04-14 16:18:44 -0700

    The LED is blinking

--- 1Lab/lab1_part3.c
+++ 1Lab/lab1_part3.c
@@ -11,12 +11,11 @@ void main(void) {
 
    serial_init();
 
-   setXRegister(PORTB);
-   setYRegister(DDRB);
    
    while (1) {
       switch (state) {
       case CONTINUE:
+         /*
          inputByte = read_byte();
 
          if (inputByte != 255 && delayTime <= 65530) {
@@ -30,7 +29,7 @@ void main(void) {
                state = FINISH;
                break;
             }
-         }
+         }*/
 
          led_on();
 
@@ -38,10 +37,9 @@ void main(void) {
 
          led_off();
 
+         _delay_ms(100);
 
          state = CONTINUE;
-
-         _delay_ms(100);
          break;
          
       case FINISH:
@@ -56,42 +54,32 @@ void main(void) {
    }
 }
 
-void setXRegister(uint16_t value) {
-   char buffer[100];
-
-   snprintf(buffer, "ldi r27 0x%x", value >> 8, 100);
-   asm volatile (buffer);
-   snprintf(buffer, "ldi r26 0x%x", value && 0xFF, 100);
-   asm volatile (buffer);
-}
-
-void setYRegister(uint16_t value) {
-   char buffer[100];
-
-   snprintf(buffer, "ldi r29 0x%x", value >> 8, 100);
-   asm volatile (buffer);
-   snprintf(buffer, "ldi r28 0x%x", value && 0xFF, 100);
-   asm volatile (buffer);
-}
-
 void led_on() {
-   asm volatile ("ld r1 (x)");
-   asm volatile ("andi r1 0x10");
-   asm volatile ("st (x) r1");
+   asm volatile("ldi r31, 0x0");
+   asm volatile("ldi r30, 0x25");
+   asm volatile ("ld r17, z");
+   asm volatile ("andi r17, 0x10");
+   asm volatile ("st z, r17");
 
-   asm volatile ("ld r1 (y)");
-   asm volatile ("andi r1 0x10");
-   asm volatile ("st (y) r1");
+   asm volatile("ldi r31, 0x0");
+   asm volatile("ldi r30, 0x24");
+   asm volatile ("ld r17, z");
+   asm volatile ("andi r17, 0x10");
+   asm volatile ("st z, r17");
 }
 
 void led_off() {
-   asm volatile ("ld r1 (x)");
-   asm volatile ("ori r1 0xef");
-   asm volatile ("st (x) r1");
+   asm volatile("ldi r31, 0x0");
+   asm volatile("ldi r30, 0x25");
+   asm volatile ("ld r17, z");
+   asm volatile ("ori r17, 0xef");
+   asm volatile ("st z, r17");
 
-   asm volatile ("ld r1 (y)");
-   asm volatile ("ori r1 0xef");
-   asm volatile ("st (y) r1");
+   asm volatile("ldi r31, 0x0");
+   asm volatile("ldi r30, 0x24");
+   asm volatile ("ld r17, z");
+   asm volatile ("ori r17, 0xef");
+   asm volatile ("st z, r17");
 }
 
 
