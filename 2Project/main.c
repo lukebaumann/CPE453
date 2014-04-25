de0389519dfeaefd128bc1dcc057c406e679af7f
iamorphen <tyler@orphen.me>
2014-04-24 23:43:34 -0700

    Updated led_on and led_off to work with the Arduino Uno R3.

--- 2Project/main.c
+++ 2Project/main.c
@@ -49,28 +49,65 @@ void blink(void) {
 
 // Load the address of DDRB and PORTB into the Z register, then set the
 // fifth bit of each to turn on the LED
-void led_on() {
-   asm volatile("ldi r31, 0x00");
-   asm volatile("ldi r30, 0x25");
-   asm volatile("ldi r18, 0x20");
-   asm volatile("st z, r18");
+// void led_on() {
+//    asm volatile("ldi r31, 0x00");
+//    asm volatile("ldi r30, 0x25");
+//    asm volatile("ldi r18, 0x20");
+//    asm volatile("st z, r18");
 
-   asm volatile("ldi r31, 0x00");
-   asm volatile("ldi r30, 0x24");
-   asm volatile("ldi r18, 0x20");
-   asm volatile("st z, r18");
-}
+//    asm volatile("ldi r31, 0x00");
+//    asm volatile("ldi r30, 0x24");
+//    asm volatile("ldi r18, 0x20");
+//    asm volatile("st z, r18");
+// }
 
 // Load the address of DDRB and PORTB into the Z register, then clear the
 // fifth bit of each to turn on the LED
-void led_off() {
-   asm volatile("ldi r31, 0x00");
-   asm volatile("ldi r30, 0x25");
-   asm volatile ("ldi r18, 0x00");
-   asm volatile ("st z, r18");
+// void led_off() {
+//    asm volatile("ldi r31, 0x00");
+//    asm volatile("ldi r30, 0x25");
+//    asm volatile ("ldi r18, 0x00");
+//    asm volatile ("st z, r18");
 
-   asm volatile("ldi r31, 0x00");
-   asm volatile("ldi r30, 0x24");
-   asm volatile("ldi r18, 0x00");
-   asm volatile ("st z, r18");
+//    asm volatile("ldi r31, 0x00");
+//    asm volatile("ldi r30, 0x24");
+//    asm volatile("ldi r18, 0x00");
+//    asm volatile ("st z, r18");
+// }
+
+void led_on() {
+   //Set data direction to OUTPUT
+   //Clear Z high byte
+   __asm__ volatile ("clr r31");
+   //Set Z low byte to DDRB
+   __asm__ volatile ("ldi r30,0x24");
+   //Set 0x20 (DDRB bit 5 to 1)
+   __asm__ volatile ("ldi r18,0x20");
+   //Write 0x20 to location 0x24 (set LED pin as output)
+   __asm__ volatile ("st Z, r18");
+
+   //Set LED to ON
+   //Set Z low byte to LED register
+   __asm__ volatile ("ldi r30,0x25");
+   
+   __asm__ volatile ("ldi r18,0x20");
+   __asm__ volatile ("st Z, r18");
+}
+
+void led_off() {
+   //Set data direction to OUTPUT
+   //Clear Z high byte
+   __asm__ volatile ("clr r31");
+   //Set Z low byte to DDRB
+   __asm__ volatile ("ldi r30,0x24");
+   //Set 0x20 (DDRB bit 5 to 1)
+   __asm__ volatile ("ldi r18,0x20");
+   //Write 0x20 to location 0x24 (set LED pin as output)
+   __asm__ volatile ("st Z, r18");
+
+   //Set LED to OFF
+   __asm__ volatile ("ldi r30,0x25");
+   
+   __asm__ volatile ("ldi r18,0x00");
+   __asm__ volatile ("st Z, r18");
 }
