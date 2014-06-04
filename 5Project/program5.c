70346eeb4d9d8c5d1e64f08a5babeb19340f6dc5
Luke Baumann <mydoghasissues@gmail.com>
2014-06-03 17:13:28 -0700

    Started doing some ext2 reading. I placed those functions at the bottom of program5.c

--- 5Project/program5.c
+++ 5Project/program5.c
@@ -43,6 +43,7 @@ void main() {
 
    start_audio_pwm();
 
+   ext2_init();
    serial_init();
    os_init();
 
@@ -226,4 +227,23 @@ void reader(void) {
          mutex_unlock(readMutex);
       }
    }
+}
+
+// These functions will go in their own file later but for now will be right here
+int inodesPerGroup = 0;
+int sectorsPerGroup = 0;
+
+void ext2_init() {
+   struct ext2_super_block sb;
+
+   findSuperBlock(&sb);
+   inodesPerGroup = sb.s_inodes_per_group;
+   sectorsPerGroup = 2 * sb.s_blocks_per_group;
+
+}
+void findSuperBlock(struct ext2_super_block *sb) {
+   read_data(2 * SUPER_BLOCK_INDEX, 0, (uint8_t *) sb,
+         sizeof(struct ext2_super_block));
+
+   return;
 }
